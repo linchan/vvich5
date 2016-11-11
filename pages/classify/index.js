4e58b36897020c0ff29d0a83255b6b8215228dab
linchan <18971276@qq.com>
2016-11-11 20:20:36 +0800

    b

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -1,32 +1,92 @@
 var app = getApp()
 Page({
     data: {
-        navLeftItems: [],
-        navRightItems: [],
+        navLeftItems: [{
+                id: 1,
+                tree: {
+                    desc: '女装'
+                }
+            },{
+                id: 2,
+                tree: {
+                    desc: '男装'
+                }
+            },{
+                id: 3,
+                tree: {
+                    desc: '鞋'
+                }
+            },{
+                id: 4,
+                tree: {
+                    desc: '内衣'
+                }
+            },{
+                id: 5,
+                tree: {
+                    desc: '儿童用品'
+                }
+            },{
+                id: 6,
+                tree: {
+                    desc: '配件箱包'
+                }
+            },{
+                id: 7,
+                tree: {
+                    desc: '运动户外'
+                }
+            },{
+                id: 8,
+                tree: {
+                    desc: '美妆饰品'
+                }
+            },{
+                id: 9,
+                tree: {
+                    desc: '孕妇装'
+                }
+        }],
+        navRightItems: [{
+            tree: {
+                nodes: [{},{
+                    tree: {
+                        nodes: [{
+                            id:123216004,
+                            tree: {
+                                desc: '时尚套装'
+                            }
+                        },{
+                            id:50000697,
+                            tree: {
+                                desc: '毛针织衫'
+                            }
+                        }]
+                    }
+                }]
+            }
+        },{
+            tree: {
+                nodes: [{},{
+                    tree: {
+                        nodes: [{
+                            id:50000436,
+                            tree: {
+                                desc: 'T恤'
+                            }
+                        },{
+                            id:50011123,
+                            tree: {
+                                desc: '衬衫'
+                            }
+                        }]
+                    }
+                }]
+            }
+        }],
         curNav: 1,
 		curIndex: 0
     },
-    onLoad: function() {
-
-        var that = this
-
-        wx.request({
-            url: 'data/typebrandList.json',
-            method: 'GET',
-            data: {},
-            header: {
-                'Accept': 'application/json'
-            },
-            success: function(res) {
-                console.log(res.data);
-                that.setData({
-                    navLeftItems: res.data,
-                    navRightItems: res.data
-                })
-            }
-        })
-    },
-
     //事件处理函数
     switchRightTab: function(e) {
         let id = e.target.dataset.id,
